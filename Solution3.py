740cac4107d0c858284307a89e6e5eb43e67a4c0
Arpit Patel <arpitkpatel@yahoo.com>
2020-07-03 19:38:21 -0400

    Task Completed

---
+++ Solution3.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-division/problem
+
+import math
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(math.floor(a/b))
+    print(a/b)
