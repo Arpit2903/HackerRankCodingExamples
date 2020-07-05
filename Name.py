ebfbed0eff0233ee60a1e54020a26913985ece19
Arpit Patel <arpitkpatel@yahoo.com>
2020-07-04 20:11:58 -0400

    Coding Done

---
+++ Name.py
@@ -0,0 +1,7 @@
+def print_full_name(a,b):
+    print("Hello" +a+" "+b+"! You just delved into python" )
+
+if __name__ == '__main__':
+    fname = input("enter first name")
+    lname = input("enter last name")
+    print_full_name(fname,lname)
